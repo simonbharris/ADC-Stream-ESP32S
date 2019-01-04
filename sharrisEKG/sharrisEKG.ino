b40ef7043a8774f2dab3b929088ac573a1ef8f97
42 Robotics <42robotics@iMac-10.local>
2019-01-03 20:40:01 -0800

    Set to print alternating channels between  two labels.

--- sharrisEKG/sharrisEKG.ino
+++ sharrisEKG/sharrisEKG.ino
@@ -5,8 +5,11 @@
 // Maximum packet size for ADC to send over websocket
 #define BUFFSIZE 3000
 
+// Packet sends once there is this many samples. 80 samples sends once every ~108-110ms
+#define MAX_PACKET_SIZE 80
+
 // Maximum time to wait between sending packets (in milliseconds)
-#define INTERVAL 100
+//#define INTERVAL 100
 
 // Adafruit ads1115 ADC setup
 Adafruit_ADS1115 adsSensor(0x48);
@@ -41,7 +44,7 @@ void setup() {
   
   // Setups ADS1115 in continuous reading mode on a differential between pins 0 and 1.
   // Continuous mode allows for faster reading.
-  adsSensor.setupContinuousADC_Differential_0_1();
+  //adsSensor.setupContinuousADC_Differential_0_1();
   
   //adsSensor.setGain(GAIN_SIXTEEN);
 
@@ -61,8 +64,10 @@ void loop() {
  long timer;
  int i;
  int16_t datapacket[BUFFSIZE];
- int16_t avgarr[20];
- int16_t val;
+ int16_t avgarr1[20];
+ int16_t avgarr2[20];
+ int16_t val1;
+ int16_t val2;
  
   // Check if we have a client.
   WiFiClient client = server.available();
@@ -70,36 +75,47 @@ void loop() {
     if (client.connected() && wsServer.handshake(client))
     {
 
-      // While client is still connected.
-      while (client.connected()) 
+      // if a client connects
+      if (client.connected()) 
       {
         i = 0;
         timer = millis();
         
+        // pre-fill averaging arrays so we don't get very random values at the start of our sampling.
+        for (int j = 0; j < 20; j++)
+        {
+          avgarr1[i] = adsSensor.readADC_Differential_0_1();
+          avgarr2[i] = adsSensor.readADC_Differential_2_3();
+        }
+        
         while(client.connected())
         {
           // Do an ADC read (Differential on A0 and A1
-          val = adsSensor.getLastConversionResults();
+          val1 = adsSensor.readADC_Differential_0_1();
+          val2 = adsSensor.readADC_Differential_2_3();
           
           // Add read to an array for averaging
-          avgarr[i % 20] = val;
-          
-          //Record the current average.
-          datapacket[i++] = avg(avgarr, 20);
+          // Funky math because i is always divisible by 2 at this point
+          // Use of modulo here lets of keep overwriting the oldest data point for our averaging..
+          avgarr1[(i/2) % 20] = val1;
+          avgarr2[(i/2) % 20] = val2;
 
-          // Debug, print to serial
-          Serial.println(datapacket[i-1]);
-          
-          // A more decaying average result for testing.
-          //datapacket[i++] = (0.2 * val) + (0.8 * avg(avgarr, 20));
+          //Record the average of the last 20 samples..
+          //datapacket[i++] = avg(avgarr1, 20);
+          //datapacket[i++] = avg(avgarr2, 20);
+
+          // Very basic decaying average version.
+          datapacket[i++] = (0.3 * val1) + (0.7 * avg(avgarr1, 20));
+          datapacket[i++] = (0.3 * val2) + (0.7 * avg(avgarr2, 20));
           
           // Sends a datapacket at every interval, or when the buffer is full. ads1115 should send every 100ms in 85 or 86 16-bit integer packets.
-          if (timer + INTERVAL < millis() || BUFFSIZE <= i)
+          if (MAX_PACKET_SIZE <= i)
           {
              wsServer.sendData(datapacket, i);
              //Serial.println(i);
              // Reset values
              i = 0;
+             Serial.println(millis() - timer);
              timer = millis();
           }
         }
--- webSocketClient.py
+++ webSocketClient.py
@@ -1,4 +1,6 @@
+import argparse, time, os
 import websocket
+import pandas as pd
 from struct import *
 
 try:
@@ -7,16 +9,62 @@ except ImportError:
     import _thread as thread
 import time
 
-# Fo counting number of frames
-counter = 0
+# globals ########################
+g_iter = 0
+file_iter = 0
+label_iter = 0
+sample_data = []
+
+# Number of frames before saving (~110ms per frame)
+INTERVAL = 20
+
+CHANNELS = ['ch1', 'ch2']
+CH_DATA = {ch: [] for ch in CHANNELS}
+NB_CHANNELS = len(CH_DATA.keys())
+
+LABELS = ["OneFinger", "TwoFinger"]
+LABEL_COUNT = {label:0 for label in LABELS}
+NB_LABELS = len(LABELS)
+
+# Collects data from incoming message.
+def collectData(message):
+    global CH_DATA
+
+    # For each channel, pull one 2-byte integer out of the message, Keep reading the message until the end.
+    # i tracks the start position for the set of channels.
+    # x tracks which channel we are reading from.
+    for i in range(0, len(message), NB_CHANNELS * 2):
+        for x in range(NB_CHANNELS):                            # 'i'th set, 'x'th channel
+            CH_DATA['ch{}'.format(x + 1)].append(unpack("h", message[i+(x*2):i+(x*2)+2])[0])
 
 # What to do when we receive a frame.
 def on_message(ws, message):
-	global counter
+    global g_iter
+    global label_iter
+    global LABEL_COUNT
+    global CH_DATA
+    global file_iter
+
+    label = LABELS[label_iter]
+    # A new sample set starts at every INERVAL number of frames.
+    if g_iter % INTERVAL == 0:
+        print("Frame start: {} - {}  ".format(label, LABEL_COUNT[label]))
 	# We receive the data in binary, so we must unpack it. Specifying the array elementa [0] drops the tuple so it formats better.
-	for i in range(0, len(message), 2):
-		print("{}, {}".format(counter, (unpack("h", message[i:i+2])[0])))
-		counter += 1
+    collectData(message)
+    g_iter += 1
+
+    # At every INTERVAL number of frames, save to file.
+    if g_iter % INTERVAL == 0:
+        df = pd.DataFrame(CH_DATA)
+        df.to_csv("data/{}/{}.txt".format(label, str(LABEL_COUNT[label])), ",")
+        CH_DATA = {ch: [] for ch in CHANNELS}
+        print("Produced csv no: {} lable: {}".format(file_iter + 1, label))
+        LABEL_COUNT[label] += 1
+        file_iter += 1
+        if label_iter == NB_LABELS - 1:
+            label_iter = 0
+        else:
+            label_iter += 1
 
 def on_error(ws, error):
     print(error)
@@ -24,25 +72,38 @@ def on_error(ws, error):
 # What to do when we loose connection. (Prints total frames received.)
 def on_close(ws):
     print("### closed ###")
-    print(f"Counter: {counter}")
+    print(f"Num.Frames: {g_iter}")
 
 # Once we successfully connect, keep the connection open until we close the program.
 def on_open(ws):
 	def run(*args):
-		print(",ch1")	
-		while 1:
-			time.sleep(1)
-		ws.close()
-		print("thread terminating...")
+		print(",ch1, ch2")
 	
 	thread.start_new_thread(run, ())
 
 
 if __name__ == "__main__":
     websocket.enableTrace(True)
+    # ESP32 server is hosted on 192.168.4.1:80 when connected to it's access point.
     ws = websocket.WebSocketApp("ws://192.168.4.1:80",
                               on_message = on_message,
                               on_error = on_error,
                               on_close = on_close)
     ws.on_open = on_open
     ws.run_forever()
+
+# Brainstorming --- Pseudocode
+# Every INTERVAL frames, switch labels.
+# Every INTERVAL frames, print a new file.
+# Every INTERVAL*2 frames, increment file iterator.
+
+# Pseudo
+# receive data
+# When INTERVAL frames have been collected
+#   Write to file
+#   Clear gathered data
+
+# Increment label_iter
+# When label_iter == label_Count - 1 have been collected
+#    set label_iter to 0
+#    icnrement file iter
